f52150a7b7a49fd207cef3345f0dee9a2e793934
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-03-26 19:18:45 -0400

    All tests pass

--- src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
@@ -2,6 +2,8 @@ package io.zipcoder.currencyconverterapplication;
 
 public interface ConvertableCurrency {
     default Double convert(CurrencyType currencyType) {
-        return Double.MAX_VALUE;
+        return (CurrencyType.UNIVERSAL_CURRENCY.getRate()/(this.getCurrencyType().getRate() ))
+                *(currencyType.getRate()/CurrencyType.UNIVERSAL_CURRENCY.getRate());
     }
+    CurrencyType getCurrencyType();
 }
